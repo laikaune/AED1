097c6b778b8d802697ac87c4d59da2ffb5801bd1
laila <lailakauane4@gmail.com>
2024-11-16 22:18:52 -0300

    potenciacao corrigida

--- revisaopc/calculadora.c
+++ revisaopc/calculadora.c
@@ -6,15 +6,15 @@ int soma (int numero1, int numero2);
 int subtracao(int numero1, int numero2);
 float divisao(int numero1, int numero2);
 int multiplicacao(int numero1, int numero2);
-double potencia(int base, int numero);
+double potencia(int base, int expoente);
 
 int main()
 {
-    int operacao, numero1, numero2;
+    int operacao, numero1, numero2, resultado;
     menu ();
     scanf ("%d", &operacao);
 
-    printf ("Digite os numeros a serem calculados (se for potencia base e altura): \n");
+    printf ("Digite os numeros a serem calculados (se for potencia base e expoente): \n");
     scanf ("%d", &numero1);
     scanf ("%d", &numero2);
     
@@ -22,22 +22,28 @@ int main()
     switch (operacao)
     {
     case 1:
-        soma(numero1, numero2);
+       resultado = soma(numero1, numero2);
+       printf ("%d", resultado);
 
         break;
     case 2:
-        subtracao(numero1, numero2);
+        resultado = subtracao(numero1, numero2);
+         printf ("%d", resultado);
 
         break;
     case 3:
-        divisao(numero1, numero2);
+        resultado = divisao(numero1, numero2);
+            printf ("%d", resultado);
         break;
     case 4:
-        multiplicacao(numero1, numero2);
+       resultado = multiplicacao(numero1, numero2);
+        printf ("%d", resultado);
         break;
     case 5:
-        potencia (numero1, numero2);
-
+       resultado = potencia(numero1, numero2);
+        printf ("%f", resultado);
+        
+        break;
     default:
     printf ("opcao inválida!");
         break;
@@ -53,7 +59,7 @@ void menu()
 
     printf ("Qual operaçao voce ira usar? \n");
     printf ("=========================================\n"); 
-    printf ("1. soma\n 2. subtracao\n 3. divisao\n 4. multiplicacao\n 5. potencia");
+    printf (" 1. soma\n 2. subtracao\n 3. divisao\n 4. multiplicacao\n 5. potencia\n");
 }
 
 int soma(int numero1, int numero2)
@@ -76,10 +82,8 @@ float divisao(int numero1, int numero2)
     return (float)numero1 / (float)numero2;
 }
 
-double potencia(int base, int numero)
+double potencia(int base, int expoente)
 {
-    double potenciacao;
-    potenciacao = pow((double)base, (double)numero);
-    return pow((double)base, (double)numero);
+    return pow(base, expoente);
 }
 
